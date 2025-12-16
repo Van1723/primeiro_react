1d341c1269d0c375b0e5386074fbea76bd9a50ba
Van1723 <vanessa.tecma@gmail.com>
2025-12-16 14:23:25 -0300

    practice_01_react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Item from './Item.jsx'
+
+
+function App() {
+  
+
+
+  return (
+    <>
+      <div>
+        <h1> Olá!</h1>
+          <Item nome="Café" valor={10} />
+          <Item nome="Leite" valor={7} />
+      </div>
+
+      
+        
+      
+    </>
+  
+  )
+}
+
+export default App
---
+++ src/Item.jsx
@@ -0,0 +1,20 @@
+function Item(props) {
+  
+
+  return (
+    <>
+      <div>
+      <p>Produto: {props.nome}</p>
+      <p>Preço: R$ {props.valor}</p>
+    </div>
+      
+
+      
+        
+     
+    </>
+  
+  )
+}
+
+export default Item;
